7c6383c25ab3fb79f2d60f31fbc03afdf9ed7d12
Tara Menjak Maksimović <80039017+taramenjakmaksimovic@users.noreply.github.com>
2024-10-19 23:14:39 +0200

    Update solution2.py

--- step_2/solution2.py
+++ step_2/solution2.py
@@ -1,14 +1,18 @@
 import pandas as pd
 import seaborn as sb
+import os
 import matplotlib.pyplot as plt
 
-platforms=['PS4', 'XOne', 'PC', 'WiiU']
-data_csv=pd.read_csv('../dataset.csv')
-filtered_data = data_csv[data_csv['platform'].isin(platforms)]
-count_data = filtered_data.groupby(['platform', 'genre']).size().reset_index(name='count')
-genre_order = ['Action', 'Adventure', 'Fighting', 'Misc', 'Platform', 'Puzzle', 'Racing', 'Role-Playing', 
-               'Shooter', 'Simulation', 'Sports', 'Strategy' ]
-#print(count_data) this is for the purpose of testing, see test2.py
-plot=sb.barplot(x='platform', y='count', hue='genre', data=count_data, 
-            order=platforms, hue_order=genre_order)
-plt.show()
+def load_data(dataset):
+    filepath = os.path.join('..', dataset)
+    data_csv = pd.read_csv(filepath)
+    return data_csv
+
+def create_barplot(data_csv, platforms, genre_order):
+    filtered_data = data_csv[data_csv['platform'].isin(platforms)]
+    count_data = filtered_data.groupby(['platform', 'genre']).size().reset_index(name='count')
+    
+    plot = sb.barplot(x='platform', y='count', hue='genre', data=count_data, 
+                      order=platforms, hue_order=genre_order)
+    plt.show()
+    return plot
