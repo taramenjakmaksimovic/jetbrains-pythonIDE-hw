9357c61dce62ef424911abf6af437caf38676bb9
Tara Menjak Maksimović <80039017+taramenjakmaksimovic@users.noreply.github.com>
2024-10-19 23:16:01 +0200

    Update test2.py

--- step_2/test2.py
+++ step_2/test2.py
@@ -2,24 +2,13 @@ import unittest
 import pandas as pd
 import seaborn as sb
 import matplotlib.pyplot as plt
-
-
-sample_data = {
-    'platform': ['PC', 'PC', 'PS4', 'PS4', 'WiiU', 'WiiU', 'XOne', 'XOne'],
-    'genre': ['Action', 'Adventure', 'Platform', 'Puzzle', 'Shooter', 'Sports',
-               'Fighting', 'Misc'],
-    'count': [39, 8, 9, 1, 3, 2, 5, 17]
-}
-count_data = pd.DataFrame(sample_data)
-
-
-def create_bar_plot(count_data, platforms, genre_order):
-    plot = sb.barplot(x='platform', y='count', hue='genre', data=count_data, 
-                      order=platforms, hue_order=genre_order)
-    plt.show()
-    return plot
+from solution2 import load_data, create_barplot
 
 class Test2(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.data_csv = load_data('dataset.csv')
 
     def test_create_bar_plot(self):
         platforms = ['PS4', 'XOne', 'PC', 'WiiU']
@@ -27,19 +16,17 @@ class Test2(unittest.TestCase):
                        'Puzzle', 'Racing', 'Role-Playing', 'Shooter', 
                        'Simulation', 'Sports', 'Strategy']
 
-       
-        plot = create_bar_plot(count_data, platforms, genre_order)
+        plot = create_barplot(self.data_csv, platforms, genre_order)
 
-        self.assertIsNotNone(plot, "This shouln't be None.")
+        self.assertIsNotNone(plot, "The plot should not be None.")
 
         with self.subTest('Check x axis label'):
             self.assertEqual(plot.get_xlabel(), 'platform', 
-                             "Expected x axis label to be 'platform'")
-        
+                             "Expected x axis label to be 'platform'.")
+
         with self.subTest('Check y axis label'):
             self.assertEqual(plot.get_ylabel(), 'count', 
-                             "Expected y axis label to be 'count'")
-        
+                             "Expected y axis label to be 'count'.")
 
         
 
