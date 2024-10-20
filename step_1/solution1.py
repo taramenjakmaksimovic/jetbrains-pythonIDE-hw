07e98abb5135dc34f58666451d24c58cabe62013
Tara Menjak Maksimović <80039017+taramenjakmaksimovic@users.noreply.github.com>
2024-10-20 16:58:20 +0200

    Update solution1.py

--- step_1/solution1.py
+++ step_1/solution1.py
@@ -1,6 +1,15 @@
+import os
 import pandas as pd
 
-platforms=['PS4', 'XOne', 'PC', 'WiiU']
-data_csv=pd.read_csv('../dataset.csv')
-filtered_data = data_csv[data_csv['platform'].isin(platforms)]
-count_data = filtered_data.groupby(['platform', 'genre']).size().reset_index(name='count')
+
+def load_data(dataset):
+    filepath = os.path.join('..', dataset)
+    data_csv = pd.read_csv(filepath)
+    return data_csv
+
+
+def filter_and_group(data_csv):
+    platforms = ['PS4', 'XOne', 'PC', 'WiiU']
+    filtered_data = data_csv[data_csv['platform'].isin(platforms)]
+    count_data = filtered_data.groupby(['platform', 'genre']).size().reset_index(name='count')
+    return count_data
