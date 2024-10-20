e4f4570e53fb002cccf3e0a3aba9a9d3e9e70291
Tara Menjak Maksimović <80039017+taramenjakmaksimovic@users.noreply.github.com>
2024-10-20 16:59:31 +0200

    Update test1.py

--- step_1/test1.py
+++ step_1/test1.py
@@ -1,19 +1,17 @@
 import unittest
 import os
 import pandas as pd
+from solution1 import load_data, filter_and_group
 
 
 class Test1(unittest.TestCase):
         def setUp(self):
-            self.csv_file='../dataset.csv'
+            self.csv_file='dataset.csv'
             self.platforms = ['PS4', 'XOne', 'PC', 'WiiU']
-        
-        def test_csv_file_exists(self):
-            self.assertTrue(os.path.exists(self.csv_file), 'CSV file not found.')
         
         def test_load_csv(self):
             try:
-                data_csv = pd.read_csv(self.csv_file)
+                data_csv = load_data(self.csv_file)
             except Exception as e:
                 self.fail(f'Loading CSV failed with an error: {e}')
             
@@ -21,20 +19,21 @@ class Test1(unittest.TestCase):
 
             wanted_columns = ['platform', 'genre']
             for column in wanted_columns:
-                self.assertIn(column, data_csv.columns, f'Missing wanted column: {column}')
+                self.assertIn(column, data_csv.columns, f'Missing wanted column: {column}.')
             
         def test_filtered_data(self):
-            data_csv = pd.read_csv(self.csv_file)
+            data_csv = load_data(self.csv_file)
             filtered_data = data_csv[data_csv['platform'].isin(self.platforms)]
             self.assertFalse(filtered_data.empty, 'The filtered data is empty.')
 
         def test_groupby_count(self):
-            data_csv = pd.read_csv(self.csv_file)
-            filtered_data = data_csv[data_csv['platform'].isin(self.platforms)]
-            count_data = filtered_data.groupby(['platform', 'genre']).size().reset_index(name='count')
-            self.assertIn('platform', count_data.columns)
-            self.assertIn('genre', count_data.columns)
-            self.assertIn('count', count_data.columns)
+            data_csv = load_data(self.csv_file)
+            count_data=filter_and_group(data_csv)
+            self.assertIn('platform', count_data.columns, 'Expected column platform.')
+            self.assertIn('genre', count_data.columns, 'Expected column genre.')
+            self.assertIn('count', count_data.columns, 'Expected colunmn count.')
+
+             
 
              
 
